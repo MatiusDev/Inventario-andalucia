d47c89cc2de76674728e89a86277e7dc8063431d
Mateo Monsalve <mateomonsalve502@gmail.com>
2024-11-24 10:05:43 -0500

    Cambios de configuración en backend (#22)
    * Modificación: Cambiar el nombre del proyecto
    
    * Modificación: Se agregan excepciones para el frontend
    
    * Nuevo: Creando la estructura del frontend
    
    * Mod: Cambiando nombre de la carpeta del front
    
    * Modify: Agregando otras rutas de entornos de trabajo
    
    * Nuevo: Creando la estructura de carpetas para el backend
    
    * Nuevo: Configurando docker para iniciar el proyecto
    
    * Mod: Se modifican algunas configuraciones importantes para docker y librerias
    
    * Nuevo: Se generan configuraciones para la conexión a la BD y las variables de entorno
    
    * Nuevo: Creando una api completa de manera sencilla para probar SQLModel y agregar pagos a la base de datos
    
    * Nuevo: Agregando configuración CORS para que permita peticiones de cualquier dominio
    
    * Nuevo: Configurando estructura de frontend, y agregando conexión inicial al backend
    
    * Nuevo: Agregando cambios de configuración general

--- inventory_api/core/main.py
+++ inventory_api/core/main.py
@@ -2,10 +2,11 @@ import os
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from dotenv import load_dotenv
 
 #Configuraciones
-from config.environtment import config_environment
+from config.environtments import config_environment
 
 #Middlewares
 from middlewares.check_origin_middleware import CheckOriginMiddleware
@@ -15,22 +16,18 @@ from routers.index import routes
 
 load_dotenv()
 
-env = os.getenv("ENVIRONMENT")
-
-config_environment(env)
+config = config_environment()
 
 app = FastAPI()
 
-origins = {"*"}
-
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=origins,
+  allow_origins=config["origins"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"]
 )
-app.add_middleware(CheckOriginMiddleware, allowed_origins=origins)
+app.add_middleware(CheckOriginMiddleware, allowed_origins=config["origins"])
 
 app.include_router(routes, prefix="/api")
 
