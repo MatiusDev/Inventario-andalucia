f7ee4548e4de73564425794b6ca958dff3602f5d
MatiusDev <mateomonsalve502@gmail.com>
2024-11-24 10:03:45 -0500

    Nuevo: Agregando cambios de configuración general

--- inventory_api/core/middlewares/check_origin_middleware.py
+++ inventory_api/core/middlewares/check_origin_middleware.py
@@ -1,20 +1,19 @@
 from typing import Awaitable, Callable
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import Response
 from starlette.types import ASGIApp
 
 class CheckOriginMiddleware(BaseHTTPMiddleware):
-  def __init__(self, app: ASGIApp, allowed_origins: set) -> None:
+  def __init__(self, app: ASGIApp, allowed_origins: list) -> None:
     self.allowed_origins = allowed_origins
     super().__init__(app)
     
   async def dispatch(self, request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
-    host = request.headers.get("host")
-    if '*' not in self.allowed_origins:
-      if host not in self.allowed_origins:
-        content = { "error": "IP origen no permitida", "status": "fail" }
+    origin = request.headers.get("origin")
+    if origin not in self.allowed_origins:
+      content = { "error": "IP origen no permitida", "status": "fail" }
       return JSONResponse(content=content, status_code=403)
     
     response = await call_next(request)
