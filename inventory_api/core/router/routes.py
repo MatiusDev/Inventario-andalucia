6326747f1b71a168239e3cfa5c1120d452e2b3f1
Mateo Monsalve <mateomonsalve502@gmail.com>
2024-11-26 20:19:57 -0500

    Cambios en Products (Controller y Service), Fix ejecución por fallo en router (#32)
    * Nuevo: Agregando paquetes para manejar JWT
    
    * Nuevo: Se cambian algunos ajustes del router, y se crea estructura base de flujo completo de la API producto
    
    * Nuevo: Agregando listar todos los productos
    
    * Fix: Arreglando el flujo del programa y agregando dependencia del servicio

--- inventory_api/core/router/routes.py
+++ inventory_api/core/router/routes.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter
 
 # Importando las vistas y los routers
 from controllers.product_controller import route as product_route
-from controllers.auth_controller import route as auth_route
+# from controllers.auth_controller import route as auth_route
 
 routes = APIRouter()
 
 routes.include_router(product_route, tags=["Products"], prefix="/products")
-routes.include_router(auth_route, tags=["Auth"], prefix="/auth")
+# routes.include_router(auth_route, tags=["Auth"], prefix="/auth")
