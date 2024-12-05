888bbf0166aa6841e44bf01d56a2753a4eb8bf6b
MatiusDev <mateomonsalve502@gmail.com>
2024-12-04 22:06:24 -0500

    Init: Creando estructura ejemplo del join de Product y Supply

--- inventory_api/core/services/supply_service.py
+++ inventory_api/core/services/supply_service.py
@@ -1,43 +1,60 @@
 from typing import Annotated
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException
 from config.db_adapter import DBSession
 from sqlmodel import select
 
 from models.entities.supply import Supply
-from models.schemas.supply import SupplyCreate, SupplyUpdate
+from models.schemas.supply import SupplyCreate, SupplyRead, SupplyUpdate
 
 class SupplyService:
     def __init__(self, db:DBSession) -> None:
         self.db = db
 
-    def newSupply(self, supply_data: SupplyCreate):
-        supply_db = Supply.model_validate(supply_data.model_dump())
-        self.db.add(supply_db)
-        self.db.commit()
-        self.db.refresh(supply_db)
-        return supply_db
+    def create(self, supply_data: SupplyCreate):
+        try:
+            new_supply = SupplyCreate.supply_dump(supply_data)
+            
+            
+            supply_db = Supply.model_validate(new_supply)
+            
+            self.db.add(supply_db)
+            self.db.commit()
+            self.db.refresh(supply_db)
+            
+            print("HEREEEEEEEEEEEEE", supply_db)
+            return SupplyRead.model_validate(supply_db)
+        except Exception as err:
+            # if err.orig == None:
+            #     raise HTTPException(status_code=500, detail=str(err))
+            
+            # err_id, err_msg = err.orig.args
+            
+            # if (err_id == 1452):
+            #     raise HTTPException(status_code=500, detail="No se ha agregado el producto en la base de datos")
+            
+            raise HTTPException(status_code=500, detail=str(err))
     
-    def list_supply(self):
+    def get_all(self):
         supplies = self.db.exec(select(Supply)).all() or []
         return supplies
     
-    def update_supply(self, id: int, supply_data: SupplyUpdate):
-        supply_db = self.db.get(Supply, id)
-        if supply_db == None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Supply doesn't exist")
-        supply_data_dict = supply_data.model_dump(exclude_unset=True)
-        supply_db.sqlmodel_update(supply_data_dict)
-        self.db.add(supply_db)
-        self.db.commit()
-        self.db.refresh(supply_db)
-        return supply_db
+    # def update_supply(self, id: int, supply_data: SupplyUpdate):
+    #     supply_db = self.db.get(Supply, id)
+    #     if supply_db == None:
+    #         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Supply doesn't exist")
+    #     supply_data_dict = supply_data.model_dump(exclude_unset=True)
+    #     supply_db.sqlmodel_update(supply_data_dict)
+    #     self.db.add(supply_db)
+    #     self.db.commit()
+    #     self.db.refresh(supply_db)
+    #     return supply_db
     
-    def delete_supply(self, id: int):
-        supply_db = self.db.get(Supply, id)
-        if supply_db == None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Supply doesn't exist")
-        self.db.delete(supply_db)
-        self.db.commit()
-        return {"message": "Supply deleted", "status": "success"}
+    # def delete_supply(self, id: int):
+    #     supply_db = self.db.get(Supply, id)
+    #     if supply_db == None:
+    #         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Supply doesn't exist")
+    #     self.db.delete(supply_db)
+    #     self.db.commit()
+    #     return {"message": "Supply deleted", "status": "success"}
     
 SSupplyDependency = Annotated[SupplyService, Depends(SupplyService)]
