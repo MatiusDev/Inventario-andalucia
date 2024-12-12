f2d858e9854e7191a8cc4e396f8ccf80bf887959
MatiusDev <mateomonsalve502@gmail.com>
2024-12-11 21:53:20 -0500

    New: Creando rutas entre componentes y creando componentes que usan componente generico

--- app/src/dashboard/components/Sidebar/Sidebar.jsx
+++ app/src/dashboard/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,8 @@
 // css
 import "./Sidebar.css";
 
+import { useState } from "react";
+
 // componentes
 import HeadIcon from "../HeadIcon/HeadIcon";
 import Nav from "../Nav/Nav";
@@ -11,16 +13,37 @@ import PanelUser from "../PanelUser/PanelUser";
 import LogOut from "../LogOut/LogOut";
 
 // formulario
-import Notification from "../Forms/Notification/Notification"
-import UserContent from "../Forms/Content/UserContent";
+// import Notification from "../Base/Notification/Notification"
+import UserContent from "../Base/Content/UserContent";
+import SupplierContent from "../Base/Content/SupplierContent";
+import ProductContent from "../Base/Content/ProductContent";
+import OrderContent from "../Base/Content/OrderContent";
+
+const menuNavOptions = {
+    "/users": <UserContent />,
+    "/suppliers": <SupplierContent />,
+    "/products": <ProductContent />,
+    "/orders": <OrderContent />,
+    // "/tools": <ProductContent />,
+    // "/plants": <ProductContent />,
+    // "/supplies": <ProductContent />,
+    // "/customers
+    // "/notifications": <Notification />,
+}
 
 const Sidebar = () => {
+    const [navOption, setNavOption] = useState("/users");
+
+    const handleMenuClick = (option) => {
+        setNavOption(option);
+    }
+
     return (
         <div>
             <Menu />
             <div className="sidebar">
                 <HeadIcon />
-                <Nav />
+                <Nav handleMenuClick={handleMenuClick} />
                 <div>
                     <Line />
                     <DarkMode />
@@ -31,7 +54,7 @@ const Sidebar = () => {
                 </div>
             </div>
             <main>
-                <UserContent/>
+                {menuNavOptions[navOption]}
             </main>
         </div>
     );
