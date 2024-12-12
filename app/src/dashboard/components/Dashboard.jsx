b8e071741c73ff682f84b0443e923499128932bd
MatiusDev <mateomonsalve502@gmail.com>
2024-12-12 00:18:57 -0500

    Changes: Arreglando toda la ruta inicial del dashboard, agregando funcionamiento de usuario y creando modal de ejemplo

--- app/src/dashboard/components/Dashboard.jsx
+++ app/src/dashboard/components/Dashboard.jsx
@@ -1,5 +1,67 @@
+import "./Sidebar/Sidebar.css";
+import "./Dashboard.css";
+
+import { useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+
 import Sidebar from "./Sidebar/Sidebar";
 
-const Dashboard = () => (<Sidebar/>);
+// Opciones de menu
+// import Notification from "../Base/Notification/Notification"
+import UserContent from "./Base/Content/UserContent";
+import SupplierContent from "./Base/Content/SupplierContent";
+import ProductContent from "./Base/Content/ProductContent";
+import OrderContent from "./Base/Content/OrderContent";
+
+import { useNavigate } from "react-router";
+
+import { apiPost } from "@utils/api";
+
+const menuNavOptions = {
+  "/users": <UserContent />,
+  "/suppliers": <SupplierContent />,
+  "/products": <ProductContent />,
+  "/orders": <OrderContent />,
+  // "/tools": <ProductContent />,
+  // "/plants": <ProductContent />,
+  // "/supplies": <ProductContent />,
+  // "/customers:
+  // "/notifications": <Notification />,
+}
+
+const Dashboard = () => {
+    const [navOption, setNavOption] = useState("/users");
+    const navigate = useNavigate();
+
+    const handleMenuClick = (option) => {
+        setNavOption(option);
+    }
+
+    const handleLogout = async () => {
+      const URL = '/auth/logout';
+      const response = await apiPost(URL);
+      
+      if (response["status"] == "success") {
+        navigate('/', { replace: true });
+      } else {
+        console.log("Error al cerrar sesión")
+      }
+    }
+
+    return (
+      <div className="dashboard">
+        <div className="menu">
+            <FontAwesomeIcon className="icon" icon={faBars} />
+            <FontAwesomeIcon className="icon" icon={faX} />
+        </div>
+        <Sidebar handleMenuClick={handleMenuClick} handleLogout={handleLogout} />
+        <main>
+          {menuNavOptions[navOption]}
+        </main>
+      </div>
+    );
+};
 
 export default Dashboard;
--- app/src/dashboard/components/LogOut/LogOut.jsx
+++ app/src/dashboard/components/LogOut/LogOut.jsx
@@ -4,13 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
 
-const LogOut = () => {
+const LogOut = ({ handleLogout }) => {
     return (
         <li className="log-out">
-            <a>
+            <button className="button" onClick={handleLogout}>
                 <FontAwesomeIcon className="icon" icon={faPowerOff} />
                 <span>Cerrar sesion</span>
-            </a>
+            </button>
         </li>
     );
 };
