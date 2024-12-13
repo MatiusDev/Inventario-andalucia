1de98449af97867c18940f4f3c03ad05c60e03c9
juanr8877 <juanr8877@gmail.com>
2024-12-12 19:30:49 -0500

    actualizacion css

--- app/src/dashboard/components/Nav/Nav.jsx
+++ app/src/dashboard/components/Nav/Nav.jsx
@@ -2,11 +2,12 @@ import "./Nav.css"
 
 import SidebarButton from "../SidebarButton/SidebarButton";
 
-import { faEnvelope, faTruck, faBoxesStacked, faUsers, faUsersLine, faArrowRotateLeft, faTrowel, faNetworkWired, faBoxesPacking, faChartPie } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope,faFan,faDolly, faTruck,faBoxesPacking,faBoxesStacked, faUsers, faUsersLine, faArrowRotateLeft, faTrowel, faNetworkWired, faChartPie } from "@fortawesome/free-solid-svg-icons";
 import { faAudible } from "@fortawesome/free-brands-svg-icons"
 
 const Nav = ({ handleMenuClick }) => {
     return (
+        
         <nav className="nav">
             <ul>
                 <SidebarButton handleMenuClick={handleMenuClick} path={"/notifications"} icon={faEnvelope} nameSpan={"Notificaciones"} />
@@ -14,10 +15,10 @@ const Nav = ({ handleMenuClick }) => {
                 <SidebarButton handleMenuClick={handleMenuClick} path={"/suppliers"} icon={faTruck} nameSpan={"Proveedores"} />
                 {/* <SidebarButton handleMenuClick={handleMenuClick} path={"/"} icon={faBoxesPacking} nameSpan={"Inventario"} /> */}
                 <SidebarButton handleMenuClick={handleMenuClick} path={"/products"} icon={faBoxesStacked} nameSpan={"Productos"} />
-                <SidebarButton handleMenuClick={handleMenuClick} path={"/orders"} icon={faBoxesStacked} nameSpan={"Ordenes"} />
+                <SidebarButton handleMenuClick={handleMenuClick} path={"/orders"} icon={faDolly} nameSpan={"Ordenes"} />
                 <SidebarButton handleMenuClick={handleMenuClick} path={"/tools"} icon={faTrowel} nameSpan={"Herramientas"} />
-                <SidebarButton handleMenuClick={handleMenuClick} path={"/plants"} icon={faNetworkWired} nameSpan={"Plantas"} />
-                <SidebarButton handleMenuClick={handleMenuClick} path={"/supplies"} icon={faNetworkWired} nameSpan={"Insumos"} />
+                <SidebarButton handleMenuClick={handleMenuClick} path={"/plants"} icon={faFan} nameSpan={"Plantas"} />
+                <SidebarButton handleMenuClick={handleMenuClick} path={"/supplies"} icon={faBoxesPacking} nameSpan={"Insumos"} />
                 {/* <SidebarButton handleMenuClick={handleMenuClick} path={"/customers"} icon={faUsers} nameSpan={"Clientes"} /> */}
                 {/* <SidebarButton handleMenuClick={handleMenuClick} path={"/notifications"} icon={faArrowRotateLeft} nameSpan={"Devoluciones"} /> */}
                 {/* <SidebarButton handleMenuClick={handleMenuClick} path={"/notifications"} icon={faAudible} nameSpan={"Auditoria"} /> */}
